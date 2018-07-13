7e6ba849349a7aa59e066b5187de768902a89505
LucaTorriani <luca.torriani94@gmail.com>
2018-07-13 21:19:38 +0200

    Creation of the headers: proxy and factory

---
+++ src/Proxy.hpp
@@ -0,0 +1,68 @@
+#ifndef GENERICPROXY_HPP_
+#define GENERICPROXY_HPP_
+#include <string>
+#include <memory>
+#include <iostream>
+#include <type_traits>
+namespace GenericFactory {
+  /*! A simple proxy for registering into a factory.
+
+    It provides the builder as static method
+    and the automatic registration mechanism.
+
+    \param Factory The type of the factory.
+    \param ConcreteProduct is the derived (concrete) type to be
+    registered in the factory
+
+    @note I have to use the default builder provided by the factory. No check is made to verify it
+    @todo Add check un builder type using type_traits and static_assert
+  */
+  template
+  <typename Factory, typename ConcreteProduct>
+  class Proxy {
+  public:
+
+    typedef typename  Factory::AbstractProduct_type AbstractProduct_type;
+    typedef typename  Factory::Identifier_type Identifier_type;
+    typedef typename  Factory::Builder_type Builder_type;
+    // The type returned by the builder
+    typedef typename  std::result_of<Builder_type()>::type Result_type;
+    typedef           Factory Factory_type;
+
+    //! The constructor does the registration.
+    Proxy(Identifier_type const &);
+
+    //! The builder. Must comply with the signature.
+    /*!
+      Actually you do not need to use the proxy to define the builder, you may do it in a simpler way.
+      I assume that the builder is type that can be constructed from a function with signature
+      Result_type () and that Result_type can be constructed with a pointer ot the concrete product.
+
+      \todo add a static assert with type trait is_same to test if the builder defined in the
+      factory complies with this.
+     */
+    static Result_type Build()
+    {
+      return Result_type(new ConcreteProduct());
+    }
+    //    static std::unique_ptr<AbstractProduct_type> Build(){
+    //return std::unique_ptr<AbstractProduct_type>(new ConcreteProduct());
+    //}
+
+  private:
+    Proxy(Proxy const &)=delete; // only C++11
+    Proxy & operator=(Proxy const &)=delete; // only C++11
+  };
+
+
+  template<typename F, typename C>
+  Proxy<F,C>::Proxy(Identifier_type const & name) {
+    // get the factory. First time creates it.
+    Factory_type & factory(Factory_type::Instance());
+    // Insert the builder. The & is not needed.
+    factory.add(name,&Proxy<F,C>::Build);
+    std::cout<<"Added "<< name << " to factory"<<std::endl;
+  }
+}
+
+#endif /* RULESHANDLER_HPP_ */
