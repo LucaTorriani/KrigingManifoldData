7d39982f6b805d0110112614c376c5364663e600
LucaTorriani <luca.torriani94@gmail.com>
2018-07-13 21:17:06 +0200

    Updated using map

--- src/Distance.cpp
+++ src/Distance.cpp
@@ -2,12 +2,14 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-static double Distance::eucl_dist(const Point& P1, const Point& P2){
+
+using namespace distances;
+
+double EuclDist::operator()(const Point& P1, const Point& P2){
   return ((P1-P2).l2norm());
 }
 
-
-static double Distance::geo_dist(const Point& P1, const Point& P2){
+double Geodist::operator()(const Point& P1, const Point& P2){
   // Great-circle distance
   double coeff = M_PI_2/90;
   double lat1 =  P1(1);
@@ -16,40 +18,35 @@ static double Distance::geo_dist(const Point& P1, const Point& P2){
   double long2 =  P2(2);
   double cos_angle = sin(lat1*coeff)*sin(lat2*coeff)+cos(lat1*coeff)*cos(lat2*coeff)*cos(cmath::abs(long2-long1)*coeff);
 
-  if (abs(cos_angle-1) < 2*Distance::eps_dbl)
+  if (abs(cos_angle-1) < 2*eps_dbl)
     return (0);
-  else if (abs(cos_angle+1) < 2*Distance::eps_dbl)
-    return (Distance::Earth_R * M_PI);
+  else if (abs(cos_angle+1) < 2*eps_dbl)
+    return (Earth_R * M_PI);
   else
-    return (Distance::Earth_R*acos(cos_angle));
+    return (Earth_R*acos(cos_angle));
 
 }
 
+Distance::Distance(){
+  distances.insert(std::pair<std::string, std::function<double(std::vector<double>, std::vector<double>)>>("Euclidean", EuclDist()));
+  distances.insert(std::pair<std::string, std::function<double(std::vector<double>, std::vector<double>)>>("Manhattan", Manhattan()));
 
+}
+double Distance::compute_distance(const Point& P1, const Point& P2, const std::string & distance_type){
+  double result = dist[distance_type](P1, P2);
+  return result;
+}
 
-static SpMat Distance::create_distance_matrix(const &std::vector<Point> coords, checkDistance){
+SpMat Distance::create_distance_matrix(const std::vector<Point> & coords,const std::string & distance_type){
   size_t num_points = coords.size();
 
   std::vector<TripType> tripletList;
   tripletList.reserve(num_points*(num_points-1)/2);
-  switch (checkDistance){
-    case EUCLIDEAN: {
-      for (size_t i=0; i<(num_points-1); i++ ) {
-        for (size_t j=(i+1); j<num_points; j++ ) {
-          tripletList.push_back(TripType(i,j,Distance::eucl_dist(coords[i], coords[j])));
-        }
-      }
+  for (size_t i=0; i<(num_points-1); i++ ) {
+    for (size_t j=(i+1); j<num_points; j++ ) {
+      tripletList.push_back(TripType(i,j,compute_distance(coords[i], coords[j], distance_type)));
     }
-    break;
-    case GEODIST: {
-      for (size_t i=0; i<(num_points-1); i++ ) {
-        for (size_t j=(i+1); j<num_points; j++ ) {
-          tripletList.push_back(TripType(i,j,Distance::geo_dist(coords[i], coords[j])));
-        }
-      }
-    }
-    break;
-  };
+  }
 
   SpMat distance_matrix(num_points, num_points);
   distance_matrix.setFromTriplets(tripletList.begin(), tripletList.end());
--- src/Distance.hpp
+++ src/Distance.hpp
@@ -3,13 +3,45 @@
 
 #include "Point.hpp"
 #include "Helpers.hpp"
+#include <vector>
+#include <utility>
+#include <map>
+#include <functional>
 
-struct Distance{
-  static double eucl_dist(const Point&, const Point&);
-  static double geo_dist(const Point&, const Point&);
-  static SpMat create_distance_matrix(const std::vector<Point>&, checkDistance);
-  static const double Earth_R(6371.0);
-  static const double eps_dbl(std::numeric_limits<double>::epsilon()); 
+namespace distances{
+
+class EuclDist{
+public:
+      double operator()(const Point&, const Point& );
+  };
+
+class GeoDist{
+  double constexpr Earth_R = 6371.0; //constrollare
+  double constexpr eps_dbl = std::numeric_limits<double>::epsilon; //constrollare
+
+public:
+      double operator()(const Point&, const Point&);
 };
 
+class Distance{
+  std::map<std::string,std::function<double(std::vector<double>, std::vector<double>)>> dist;
+public:
+  Distance();
+  double compute_distance(const Point&, const Point&, const std::string &);
+  SpMat create_distance_matrix(const std::vector<Point> &, const std::string & );
+
+};
+
+
+
+}
+
+
+// struct Distance{
+//   static double eucl_dist(const Point&, const Point&);
+//   static double geo_dist(const Point&, const Point&);
+//   static SpMat create_distance_matrix(const std::vector<Point>&, checkDistance);
+//   static const double Earth_R(6371.0);
+//   static const double eps_dbl(std::numeric_limits<double>::epsilon());
+// };
 #endif
