56195bb5a06d870377cb6d131845763169ed72d7
Ilaria Sartori <10470560@polimi.it>
2018-09-07 13:51:32 +0200

    Implemented class EmpiricalVariogram and GaussVariogram

--- src/Variogram.hpp
+++ src/Variogram.hpp
@@ -6,49 +6,55 @@
 namespace VariogramEvaluation{
 
 class EmpiricalVariogram {
-  Vec _emp_vario_values;
-  Vec _hvec;
-  double compute_hmax() const;
+  //Variano
+  std::vector<double> _emp_vario_values;
+  std::vector<double> _hvec;
+  std::vector<unsigned int> _N_hvec;
+  // Costanti
+  double _N;
+  Vec _d; // Vettore h+-deltah
+  double _hmax;
+  const DistanceTplane _distanceTplane;
+  const SpMat _distanceMatrix;
+  Vec _weights;
+
+  void compute_hmax();
 
 public:
   Vec get_emp_vario_values () const;
+  Vec get_N_hvec() const;
   Vec get_hvec() const;
-  EmpiricalVariogram(/*residui, n_h*, il resto sono static members*/);
+  void update_emp_vario(/*residui*/);
 }
 
 class FittedVariogram{
 protected:
-  Vec parameters; // tau2, sigma2, a
-  void get_init_par(const EmpiricalVariogram &)) = 0;
-  double compute_jacobian(const Vec &) const = 0;
+  double weighted_median (const Vec &, const Vec &);
+  Vector3d _parameters; // tau2, sigma2, a
+  virtual void get_init_par(const EmpiricalVariogram &)) = 0;
   void backtrack(const Vec &,Vec &,Vec &,Matrixd &,const Vec &, double, double, std::function<Vec()>);
+  virtual MatrixXd compute_jacobian(const Vec &) const = 0;
 
 public:
   double get_tau2() const;
   double get_sigma2() const;
   double get_a() const;
   void evaluate_par_fitted(const EmpiricalVariogram &);
-  double get_vario_univ(const double &) const = 0;
-  double get_covario_univ(const double &) const = 0;
-
-  Vec get_vario_vec(const Vec &) const = 0;
-  Vec get_covario_vec(const Vec &) const = 0;
-
+  virtual double get_vario_univ(const double &) const = 0;
+  virtual double get_covario_univ(const double &) const = 0;
+  virtual Vec get_vario_vec(const Vec &) const = 0;
 
 }
 
 
 class GaussVariogram : public FittedVariogram {
-
-  void get_init_par() override;
-  double compute_jacobian(const double &) const;
+  void get_init_par(const EmpiricalVariogram &) override;
+  MatrixXd compute_jacobian(const Vec &) const;
 
 public:
-
-  void update_par_fitted() override;
   double get_vario_univ(const double &) const override;
   double get_covario_univ(const double &) const override;
-
+  Vec get_vario_vec(const Vec &) const override;
 }
 
 }
