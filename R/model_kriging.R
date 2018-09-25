d70fbdfc2fdc2680ded13c9a0378e58792389516
LucaTorriani <luca.torriani94@gmail.com>
2018-09-25 11:44:56 +0200

    Added boolean for plot

--- R/model_kriging.R
+++ R/model_kriging.R
@@ -6,7 +6,7 @@
 model_kriging = function(data_manifold, coords,X = NULL, Sigma, metric_manifold="Frobenius",
                                  metric_ts = "Frobenius", model_ts="additive", vario_model="Gaussian",
                                  n_h=15, distance="Geodist", max_it = 100, tolerance = 10^(-4), weight=NULL,
-                                 new_coords, X_new = NULL){
+                                 new_coords, X_new = NULL, plot = TRUE){
 
   if ( distance == "Geodist" & dim(coords)[2] != 2){
     stop("Geodist without two coordinates")
@@ -23,8 +23,11 @@ model_kriging = function(data_manifold, coords,X = NULL, Sigma, metric_manifold=
   empirical_variogram = list(emp_vario_values = result$emp_vario_values, h = result$h_vec)
   fitted_variogram = list(fit_vario_values = result$fit_vario_values, hh = result$hh)
 
-  plot_variogram(empirical_variogram = empirical_variogram, fitted_variogram = fitted_variogram, model = vario_model,
+  if(plot){
+    plot_variogram(empirical_variogram = empirical_variogram, fitted_variogram = fitted_variogram, model = vario_model,
                  distance = distance)
+  }
+
 
   return (list(beta_opt = result$beta, gamma_matrix = result$gamma_matrix, residuals = result$residuals,
                par = result$vario_parameters, iter = result$iterations, prediction = result$prediction))
