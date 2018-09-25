f9700f5e9069a236a004faf07cfc9063391501f3
LucaTorriani <luca.torriani94@gmail.com>
2018-09-25 10:24:05 +0200

    Trying to fix package bug

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onAttach <- function(libname, pkgname)
   packageStartupMessage("Manfidolgstat 0.1.23 loaded\n")
 
-.onUnload <- function(libpath)
+.onUnload <- function(libpath){
   library.dynam.unload("Manfidolgstat",  libpath)
+}
