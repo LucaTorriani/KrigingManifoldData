94eddd3014b202e2bcae4a8b4b3b45775aad37e6
Ilaria Sartori <10470560@polimi.it>
2018-09-26 23:53:58 +0200

    Testing

--- tests/RandomField-cov-rect-tan.R
+++ tests/RandomField-cov-rect-tan.R
@@ -3,8 +3,9 @@ load("/vagrant/KrigingManifoldData/data/RandomSample-Cov-rect-tan.Rdata")
 coords_tot = gridCov
 data_manifold_tot = fieldCov
 
+dyn.load("/vagrant/KrigingManifoldData/src/interface_function.so")
 
-source ("/vagrant/KrigingManifoldData/R/intermediate_functions.R")
+# source ("/vagrant/KrigingManifoldData/R/intermediate_functions.R")
 
 ### Model parameters
 metric_manifold = "Frobenius"      # "LogEuclidean" or "SquareRoot" or "Frobenius"
@@ -37,13 +38,74 @@ coords_model = rGrid    # Sample of the grid
 ##############################################################################
 ################################## MODEL #####################################
 ##############################################################################
-if (FALSE) {
-beta_gamma_opt = model_GLS_sigma_fixed(data_manifold = data_manifold_model, coords = coords_model,  
+
+model_GLS = function(data_manifold, coords, X = NULL, Sigma = NULL, metric_manifold="Frobenius",
+                     metric_ts = "Frobenius", model_ts="additive", vario_model="Gaussian",
+                     n_h=15, distance="Geodist", max_it = 100, tolerance = 1e-6, weight_vario=NULL,
+                     weights_intrinsic = NULL, tolerance_intrinsic = 1e-6, plot = TRUE){
+  
+  if ( distance == "Geodist" & dim(coords)[2] != 2){
+    stop("Geodist without two coordinates")
+  }
+  
+  if( is.array(data_manifold_model)){
+    data_manifold = alply(data_manifold,3)
+  }
+  
+  if(!is.null(X)) {X = as.matrix(X)}
+  if(is.null(Sigma)){
+    if(is.null(weights_intrinsic)) weights_intrinsic = rep(1, length(data_manifold))
+  }
+  
+  coords = as.matrix(coords)
+  result =.Call("get_model",data_manifold, coords,X, Sigma, distance, metric_manifold, metric_ts, model_ts, vario_model,
+                n_h, max_it, tolerance, weight_vario, weights_intrinsic, tolerance_intrinsic )
+  
+  
+  empirical_variogram = list(emp_vario_values = result$emp_vario_values, h = result$h_vec)
+  fitted_variogram = list(fit_vario_values = result$fit_vario_values, hh = result$hh)
+  
+  if(plot){
+    plot_variogram(empirical_variogram = empirical_variogram, fitted_variogram = fitted_variogram, model = vario_model,
+                   distance = distance)
+  }
+  #class(result) <- "modelGLS"
+  
+  return (result)
+}
+
+plot_variogram = function (empirical_variogram, fitted_variogram, model, distance) {
+  hh = fitted_variogram$hh
+  plot(hh[2:length(hh)],fitted_variogram$fit_vario_values[2:length(hh)],  col = 'blue', type = 'l',
+       ylim = c(0,1.15*max(empirical_variogram$emp_vario_values)), ylab = model, xlab = distance)
+  points(empirical_variogram$h, empirical_variogram$emp_vario_values, pch = 4, col = 'blue')
+  
+}
+
+
+beta_gamma_opt = model_GLS(data_manifold = data_manifold_model, coords = coords_model,  
                                        Sigma = Sigma, distance = distance, metric_manifold = metric_manifold, 
                                        metric_ts = metric_ts, model_ts = model_ts,
                                        vario_model = vario_model, n_h = 15,  
                                        max_it = 100, tolerance = 10^(-4))
+# print(beta_gamma_opt$beta)
+# print(beta_gamma_opt$vario_parameters)
+# print(beta_gamma_opt$iterations)
 
+
+# Rcpp::List result = Rcpp::List::create(Rcpp::Named("beta") = beta_vec_matrices,
+#                                        Rcpp::Named("fit_vario_values") = fit_vario_values,
+#                                        Rcpp::Named("hh") = hh,
+#                                        Rcpp::Named("gamma_matrix") = gamma_matrix,
+#                                        Rcpp::Named("residuals") = resVec,
+#                                        Rcpp::Named("emp_vario_values") = emp_vario.get_emp_vario_values(),
+#                                        Rcpp::Named("h_vec") = emp_vario.get_hvec(),
+#                                        Rcpp::Named("vario_parameters") = parameters,
+#                                        Rcpp::Named("iterations") = num_iter,
+#                                        Rcpp::Named("Sigma")= Sigma);
+
+if (FALSE) {
+  
 model = list(beta_opt = beta_gamma_opt$beta, gamma_matrix = beta_gamma_opt$gamma_matrix,
              residuals = beta_gamma_opt$residuals, fitted_par_vario = beta_gamma_opt$par)
 
@@ -125,7 +187,7 @@ for(i in 1:250)
 
 }
 rect(x.min, y.min, x.max, y.max)
-}
+
 # ###############################################################################
 # ############################## Test single value ##############################
 # ###############################################################################
@@ -209,4 +271,4 @@ for(i in 1:250)
   
 }
 rect(x.min, y.min, x.max, y.max)
-
+}
