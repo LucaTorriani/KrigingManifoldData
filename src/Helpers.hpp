b5ef46e1f96183f23208f59ae53ce5f290d88a9e
Ilaria Sartori <10470560@polimi.it>
2018-10-31 13:55:51 +0100

    Bug fixing

--- src/Helpers.hpp
+++ src/Helpers.hpp
@@ -20,7 +20,7 @@ namespace matrix_manipulation {
   MatrixXd logMat(const MatrixXd&);
   MatrixXd sqrtMat(const MatrixXd&);
 
-  std::vector<MatrixXd> bigMatrix2VecMatrices(const MatrixXd&, unsigned int, std::string);
+  std::vector<MatrixXd> bigMatrix2VecMatrices(const MatrixXd&, unsigned int, const std::string&);
   MatrixXd VecMatrices2bigMatrix(const std::vector<MatrixXd>&);
   MatrixXd Chol_semidef (const MatrixXd& M1);
   MatrixXd Chol_decomposition (const MatrixXd M1);
