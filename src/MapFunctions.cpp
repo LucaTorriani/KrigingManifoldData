0333b48da8c659a6d25656df0a87c081a5565f46
Ilaria Sartori <10470560@polimi.it>
2018-11-02 14:35:36 +0100

    Added checks on maps for "Correlation" to avoid Nan

--- src/MapFunctions.cpp
+++ src/MapFunctions.cpp
@@ -59,11 +59,14 @@ MatrixXd logMapChol::map2tplane (const MatrixXd& H) const{
   Vec H_vec;
   Vec Sigma_vec;
   Vec proj_diff;
+  double proj_diff_norm;
   for (size_t i=1; i<p; i++) {
     H_vec = H.col(i);
     Sigma_vec = _Sigma.col(i);
     proj_diff = proj2tspace(H_vec-Sigma_vec, Sigma_vec);
-    result.col(i) = acos((H_vec.transpose()*Sigma_vec).value()) * proj_diff/proj_diff.norm();
+    proj_diff_norm = proj_diff.norm();
+    if (proj_diff_norm < 1e-10) result.col(i).setZero();
+    else result.col(i) = acos((H_vec.transpose()*Sigma_vec).value()) * proj_diff/proj_diff_norm;
   }
   return result;
 }
@@ -144,7 +147,8 @@ MatrixXd expMapChol::map2manifold (const MatrixXd& V) const{
   result(0,0)=1;
   for (size_t i=1; i<p; i++) {
     double col_norm (V.col(i).norm());
-    result.col(i) = cos(col_norm)* _Sigma.col(i) + sin(col_norm)*V.col(i)/col_norm;
+    if (col_norm < 1e-10) result.col(i) = _Sigma.col(i);
+    else result.col(i) = cos(col_norm)* _Sigma.col(i) + sin(col_norm)*V.col(i)/col_norm;
   }
   return result;
 }
