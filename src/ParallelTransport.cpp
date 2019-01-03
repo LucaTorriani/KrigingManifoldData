32fa82049ba2c0ea54da147e700fd0d27a99b560
Ilaria Sartori <10470560@polimi.it>
2019-01-03 16:47:11 +0100

    Changed parallel transport formulas

--- src/ParallelTransport.cpp
+++ src/ParallelTransport.cpp
@@ -2,27 +2,48 @@
 
 using namespace parallel_transport;
 
+// MatrixXd parallel_transport::transport_to_TI(MatrixXd Sigma, MatrixXd V) {
+//   unsigned int p = Sigma.rows();
+//   Eigen::LDLT<MatrixXd> solver(p);
+//   MatrixXd Id(p,p);
+//   Id.setIdentity();
+//   MatrixXd SigmaInvSqrt(p,p);
+//   MatrixXd SigmaSqrt(p,p);
+//   MatrixXd tmp(p,p);
+//
+//   SigmaSqrt = matrix_manipulation::sqrtMat(Sigma);
+//   solver.compute(SigmaSqrt);
+//   SigmaInvSqrt = solver.solve(Id);
+//   tmp = SigmaSqrt* matrix_manipulation::expMat(0.5*SigmaInvSqrt*V*SigmaInvSqrt) * SigmaInvSqrt;
+//
+//   return(tmp*Sigma*tmp.transpose());
+// }
+//
+// MatrixXd parallel_transport::transport_from_TI(MatrixXd Lambda, MatrixXd V) {
+//   unsigned int p = Lambda.rows();
+//   MatrixXd tmp(p,p);
+//   tmp = matrix_manipulation::expMat(0.5*V);
+//
+//   return(tmp*tmp.transpose());
+// }
+
 MatrixXd parallel_transport::transport_to_TI(MatrixXd Sigma, MatrixXd V) {
   unsigned int p = Sigma.rows();
   Eigen::LDLT<MatrixXd> solver(p);
-  solver.compute(Sigma);
   MatrixXd Id(p,p);
   Id.setIdentity();
   MatrixXd SigmaInvSqrt(p,p);
-  MatrixXd SigmaSqrt(p,p);
-  MatrixXd tmp(p,p);
 
+  solver.compute(Sigma);
   SigmaInvSqrt = matrix_manipulation::sqrtMat(solver.solve(Id));
-  SigmaSqrt = matrix_manipulation::sqrtMat(Sigma);
-  tmp = SigmaSqrt* matrix_manipulation::expMat(0.5*SigmaInvSqrt*V*SigmaInvSqrt) * SigmaInvSqrt;
 
-  return(tmp*Sigma*tmp.transpose());
+  return(SigmaInvSqrt*V*SigmaInvSqrt.transpose());
 }
 
-MatrixXd parallel_transport::transport_from_TI(MatrixXd Lambda, MatrixXd V) {
-  unsigned int p = Lambda.rows();
-  MatrixXd tmp(p,p);
-  tmp = matrix_manipulation::expMat(0.5*V);
+MatrixXd parallel_transport::transport_from_TI(MatrixXd Sigma, MatrixXd V) {
+  unsigned int p = Sigma.rows();
+  MatrixXd SigmaSqrt(p,p);
+  SigmaSqrt = matrix_manipulation::sqrtMat(Sigma);
 
-  return(tmp*tmp.transpose());
+  return(SigmaSqrt*V*SigmaSqrt.transpose());
 }
