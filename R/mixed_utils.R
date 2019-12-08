12a0fa09efbbe75dcc7f32e2c7fc6df314ecbdcb
Ilaria Sartori <10470560@polimi.it>
2019-12-08 10:47:20 +0000

    Revert "Bug fixing"
    This reverts commit dba3443e9a7c5d91861457e58e6e25a89b6da6e5.

--- R/mixed_utils.R
+++ R/mixed_utils.R
@@ -190,7 +190,7 @@ RDD_OOK_boot_man_mixed = function(data_coords, data_val, K, grid, nk_min, B,
   if(suppressMes)  oldw <- getOption("warn");  options(warn = -1)
   # Set parameters and initialize lists
   #*lists and parameters
-  kervalues_mean=fmean_grid=fmean_data = list() # kervalues_krig=fmean=vfit
+  fmean=kervalues_mean=list() # kervalues_krig=fmean=vfit
   # nk=rep(0,K)
   #*data
   colnames(data_coords)=c('x','y')
@@ -234,8 +234,7 @@ RDD_OOK_boot_man_mixed = function(data_coords, data_val, K, grid, nk_min, B,
     # veclocmean = list()
 
     # fpred[[b]]=matrix(NA,ngrid,num.signif.entries); colnames(fpred[[b]])=c(paste("Z",1:num.signif.entries))
-    fmean_grid[[b]]= list() 
-    fmean_data[[b]]= list() # Lista di ngrid matrici p*p
+    fmean[[b]]= list() # Lista di ngrid matrici p*p
 
     if(ker.width.intrinsic>0) {
       kervalues_mean[[b]]=matrix(NA,ngrid,1); colnames(kervalues_mean[[b]])="Ker.val.mean"
@@ -274,6 +273,7 @@ RDD_OOK_boot_man_mixed = function(data_coords, data_val, K, grid, nk_min, B,
       #   }
 
       ##### Dati
+      print(nk)
       if(nk[as.character(k)]>0)  #table(factor(assigng, levels=1:K))[as.character(k)]>0
       {
         # datak=data.frame(data[assign==k,]) # extract data in k-th neighb.
